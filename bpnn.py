36e50e64f30beeb41bd78261f280032f5abc4b91
Doski <doski@Doskis-MacBook-Pro.local>
2020-07-12 08:41:48 +0000

    Added Traffic Volume Graph

--- bpnn.py
+++ bpnn.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 # Importing the dataset
 ngsim_us101 = pd.read_csv("NGSIM_US101.csv")
+ngsim_us101 = ngsim_us101.drop('Unnamed: 0', axis=1)
 
 X = ngsim_us101.iloc[:, 0:18].values
 y = ngsim_us101.iloc[:, 18].values
@@ -59,13 +60,42 @@ cm = confusion_matrix(y_test, y_pred)
 from datetime import datetime
 time_data = ngsim_us101.loc[:, 'Global_Time'].values
 
-real_date = []
-times = []
+milli = []
 
 for date in time_data:
-    real_date.append(datetime.fromtimestamp(date/1000).strftime("%A, %B %d, %Y %I:%M:%S"))
-    times.append(datetime.fromtimestamp(date/1000).strftime("%I:%M:%S"))
+    milli.append(date)
 
-real_date.sort()
-real_date_data = pd.DataFrame(np.array(real_date))
-real_time_data = pd.DataFrame(np.array(times))
+milli.sort()
+
+beginning = milli[0]
+begin = [milli[0],]
+end = 0
+indexes = [0]
+for num in range(len(milli)):
+    while num < (len(milli) - 1):
+        if ((milli[num + 1] - beginning) < 300000):
+            end = milli[num]
+        else:
+            begin.append(beginning)
+            beginning = end
+            indexes.append(num)
+        break
+
+X = []
+Y = []
+var = 0
+
+for num in range(1, len(begin)):
+    X.append(datetime.fromtimestamp(begin[num]/1000).strftime("%I:%M"))
+    
+for num in range(len(indexes) - 1):
+    var = indexes[num + 1] - indexes[num]
+    Y.append(var)
+
+plt.figure(figsize=[20,10])
+plt.title("Volume of cars on the US101 Highway on 15-June-2005", fontsize=24)
+plt.plot(X, Y)
+plt.yticks(np.arange(30000, 90000, step=5000))
+plt.xlabel("Time of Vehicle's arrival", fontsize=14)
+plt.ylabel("Number of vehilces", labelpad=10, fontsize=14)
+plt.show()
